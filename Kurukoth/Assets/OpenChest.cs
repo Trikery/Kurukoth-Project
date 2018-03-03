7878cddb3e4b581621ff0442f1a04306983bf6d2
Kirt Hullinger <kirt.jh@gmail.com>
2018-03-02 18:28:08 -0700

    Edits to Kurukoth

---
+++ Kurukoth/Assets/OpenChest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChest : MonoBehaviour {
+
+	public bool isSubscribed = false;
+	public bool isOpen = false;
+
+	public Animator anim;
+
+	void Start(){
+		anim = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter(){
+		if (!isOpen) {
+			if (!isSubscribed) {
+				InputChecker.Activate += ChestActivationHandler;
+				isSubscribed = true;
+			}
+		}
+	}
+		
+	void ChestActivationHandler (){
+		print ("Open");
+		anim.SetTrigger ("Open");
+		InputChecker.Activate -= ChestActivationHandler;
+		isSubscribed = false;
+		isOpen = true;
+	}
+
+	void OnTriggerExit (){
+		InputChecker.Activate -= ChestActivationHandler;
+		isSubscribed = false;
+	}
+}
